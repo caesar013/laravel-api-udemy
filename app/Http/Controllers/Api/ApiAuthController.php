2b3add7b330ccdcba8ee5646a6a897f7b7fbfa3d
caesar <fikrilha099@gmail.com>
2024-02-06 11:27:12 +0700

    Login with Auth::attempt

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Resources\LoginResource;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ApiAuthController extends Controller
 {
     // login
     public function login(LoginRequest $request)
     {
-        // validate
+        // validate with Auth::attempt
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            $token = $request->user()->createToken('token')->accessToken;
+            return new LoginResource([
+                'user' => $request->user(),
+                'token' => $token
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
         // check if user exists
         // check if password is correct
         // create token
